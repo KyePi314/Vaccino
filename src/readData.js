7d94cfa3044875267118fdee690cc31066838c97
KyePi314 <106054184+KyePi314@users.noreply.github.com>
2022-11-12 18:44:15 +1300

    Login system fully functional

--- src/readData.js
+++ src/readData.js
@@ -1,18 +1,44 @@
-const fs = require('fs');
-const readline = require('readline');
+var fs=require('fs');
+let $ = require('jquery');
 
-const login = document.getElementById("submit");
-login.addEventListener('click', submitEvent);
-function submitEvent()
-{
-    fs.readFile('./backend/savedUsers.json', (err, fileContent) => {
-        if(err){
-            console.log(err);
-        }
-        else{
-            data = JSON.parse(fileContent.toString());
-            
-        }
-    })
+$(document).ready(function () {
+    $.getJSON('C:/Users/kyess/OneDrive - UP Education/Documents/testing4/my-app/backend/savedUsers.json', function(jsdata) {
+        const login = document.getElementById("submit");
+        login.addEventListener('click', submitEvent);
+        function submitEvent()
+        { 
+            const username = document.getElementById('username').value;
+            const pass = document.getElementById('password').value;
 
-}
+                const users = JSON.stringify(jsdata);
+                const validJsonStr = users.replaceAll(`'`, `"`);
+                const userDB = JSON.parse(validJsonStr);
+                // console.log(userDB[0].userData.userName);
+            try {
+                for (var i = 0; i < userDB.length; i++){
+                    if (userDB[i].userData.userName == username && userDB[i].userData.password == pass && userDB[i].userData.authLvl == "User"){
+                        console.log("Success", userDB[i].userData.authLvl);
+                        window.location.replace('userHome.html');
+                    }
+                    else if (userDB[i].userData.userName == username && userDB[i].userData.password == pass && userDB[i].userData.authLvl == "Admin") {
+                        console.log("Success", userDB[i].userData.authLvl);
+                        window.location.replace('admin.html');
+                    }
+                    else if (userDB[i].userData.userName == username && userDB[i].userData.password != pass){
+                        console.log("Invalid password");
+                    }
+                    else if (userDB[i].userData.userName != username) {
+                        console.log("Invalid username");
+                    }
+                    
+                }
+                
+                
+               } catch (err) {
+                console.log(err);
+               }
+        
+        }
+        
+    });
+ });
