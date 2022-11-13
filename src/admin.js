666f21ba2b0ff69c4cd3603ffd08509a8f5cab1c
Adrian Fry <270166086@yoobeestudent.ac.nz>
2022-11-13 20:58:14 +1300

    Updating

---
+++ src/admin.js
@@ -0,0 +1,28 @@
+//<!--This function will be used to change the covid numbers for the system and home page-->
+//function covidno(){
+
+//syncReadFile('./miscsitefunctions');
+//if(){
+//var covidswitch = 0;
+
+//  return ;
+//}
+//}
+
+//<!--This function will be used to change the alerts in the system and on the home page-->
+//function alerts(){
+
+//syncReadFile('./miscsitefunctions');
+//if(alertswitch == 0){
+//  
+
+//  return <img id="alert1" src="images\yellow.svg">;
+//}
+//else if(alertswitch == 1){
+//  return <img id="alert1" src="images\red.svg">;
+//}
+
+//else if(alertswitch == 2){
+//return <img id="alert1" src="images\green.svg">;
+//}
+//}
