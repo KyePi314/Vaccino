2a09a513f2a3f724147c29a92ededccc5a7f1a47
KyePi314 <106054184+KyePi314@users.noreply.github.com>
2022-11-14 22:08:34 +1300

    User's vaccine and test data arrys (see comment)

--- src/userPages.js
+++ src/userPages.js
@@ -1,39 +1,45 @@
 //USER PAGES JS
 
-let tempTwo = sessionStorage.getItem("user"); //retrieves the user's data stored in the sessions storage
-let userID = JSON.parse(tempTwo); //parse's the user's information and stores it in the userID variable for later use.
+var tempTwo = sessionStorage.getItem("user"); //retrieves the user's data stored in the sessions storage
+var userID = JSON.parse(tempTwo); //parse's the user's information and stores it in the userID variable for later use.
 
-window.addEventListener('load', function(){
-   
-    console.log(userID);
-});
 
 const logout = document.getElementById('signout');
-
 
 logout.addEventListener('click', () =>
 {
     window.location.replace('index.html');
 });
 
-const vaxRec = document.getElementById('vaccineRecords');
-const test = document.getElementById('testResults');
-const code = document.getElementById('qrCode');
-const profile = document.getElementById('details');
+if (document.URL.includes('userHome.html')) //code specific to the home page
+{  
+    const vaxRec = document.getElementById('vaccineRecords');
+    const test = document.getElementById('testResults');
+    const code = document.getElementById('qrCode');
+    const profile = document.getElementById('details');
 
-vaxRec.addEventListener('click', () => {
-    window.location.replace('vaccineRecords.html');
-});
+    vaxRec.addEventListener('click', () => {
+        window.location.replace('vaccineRecords.html');
+    });
 
-test.addEventListener('click', () => {
-    window.location.replace('testResults.html');
-});
+    test.addEventListener('click', () => {
+        window.location.replace('testResults.html');
+    });
 
-code.addEventListener('click', () => {
-    window.location.replace('userQrCode.html');
-});
+    code.addEventListener('click', () => {
+        window.location.replace('userQrCode.html');
+    });
 
-profile.addEventListener('click', () => {
-    window.location.replace('userProfile.html');
-});
+    profile.addEventListener('click', () => {
+        window.location.replace('userProfile.html');
+    });
 
+}
+
+
+if (document.URL.includes('src/testResults.html')) //code specific to the test results page
+{
+    nhi = userID.num; //stores the user's nhi number
+    
+    
+}
