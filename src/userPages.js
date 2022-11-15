f1769837b1891ae205d09183b39f12c745d72c64
KyePi314 <106054184+KyePi314@users.noreply.github.com>
2022-11-15 21:39:52 +1300

    added a table for test results

--- src/userPages.js
+++ src/userPages.js
@@ -1,4 +1,5 @@
 //USER PAGES JS
+const { captureRejectionSymbol } = require('events');
 var fs = require('fs');
 let $ = require('jquery');
 
@@ -29,24 +30,23 @@ if (document.URL.includes('src/userHome.html')) //code specific to the home page
     });
 
 } else{
-    const logout = document.getElementById('signout');
-    const home = document.getElementById('homebutton');
-
-    logout.addEventListener('click', () =>
-    {
-        window.location.replace('index.html');
-    });
+    
+    const home = document.getElementById('homebutton'); //handles the home button
     home.addEventListener('click', () => {
         window.location.replace('userHome.html');
     } );
 }
 
-
+const logout = document.getElementById('signout'); //handles the signout button
+logout.addEventListener('click', () =>
+    {
+        window.location.replace('index.html');
+    });
 
 if (document.URL.includes('src/testResults.html')) //code specific to the test results page
 {
     nhi = userID.num; //stores the user's nhi number
-    var testsArr = [];
+    
 
 
     $.getJSON('userInfo.json', function(jsdata) 
@@ -57,11 +57,34 @@ if (document.URL.includes('src/testResults.html')) //code specific to the test r
             var userDB = JSON.parse(validJsonStr);    //parses the json data string
         try 
         {   
+            var testsArr = [];
             for (var i = 0; i < userDB.length; i++)//loops through the data that has been pulled from the json file, for the length of it, in order to be able to do login checks
             { 
                 if (userDB[i].userProfile.NHINumber == nhi)
                 {
+                    var tests = userDB[i].userProfile.tests;
                     
+                    var html = "<table border='1'>";
+                    html+="<tr>";
+                    html+="<th>"+"Test"+"</th>";
+                    html+="<th>"+"Date"+"</th>";
+                    html+="<th>"+"Location"+"</th>";
+                    html+="<th>"+"Result"+"</th>";
+                    html+="</tr>";
+                    for (var j = 0; j < tests.length; j++)
+                    {
+                        
+                        html+="<tr>";
+                        html+="<td>"+tests[j].test+"</td>";
+                        html+="<td>"+tests[j].date+"</td>";
+                        html+="<td>"+tests[j].locat+"</td>";
+                        html+="<td>"+tests[j].result+"</td>";
+                        html+="</tr>";
+                    
+                        
+                    }
+                    html+="</table>";
+                        document.getElementById("table").innerHTML = html;
                 }           
                      
             }
