d00a09e6ed03dc541da3da17c47d3cd4c8b01316
KyePi314 <106054184+KyePi314@users.noreply.github.com>
2022-11-16 17:21:24 +1300

    added functionality to admin buttons in header

--- src/adminPages.js
+++ src/adminPages.js
@@ -3,7 +3,26 @@ var fs = require('fs');
 var tempTwo = sessionStorage.getItem("tests"); //retrieves the user's data stored in the sessions storage
 var userID = JSON.parse(tempTwo); //parse's the user's information and stores it in the userID variable for later use.
 
-fs.readFile('src/userInfo.json', 'utf-8', function(err, data){//adds the user's NHI number to the info system that handles their test results, vaccine records etc
+if (document.URL.includes('admin.html'))
+{
+
+}
+else{
+    
+    const home = document.getElementById('homebutton'); //handles the home button
+    home.addEventListener('click', () => {
+        window.location.replace('admin.html');
+    } );
+}
+
+const logout = document.getElementById('signout'); //handles the signout button
+logout.addEventListener('click', () =>
+    {
+        window.location.replace('index.html');
+    });
+
+
+/* fs.readFile('src/userInfo.json', 'utf-8', function(err, data){//adds the user's NHI number to the info system that handles their test results, vaccine records etc
     if (err) {
         console.log(err);
     }
@@ -22,5 +41,5 @@ fs.readFile('src/userInfo.json', 'utf-8', function(err, data){//adds the user's 
             }
         });
     }
-})
+}) */
 
