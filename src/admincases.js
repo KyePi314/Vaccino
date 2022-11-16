d50c5db8f5ec242107954e3e0d69920ee8e9d6eb
KyePi314 <106054184+KyePi314@users.noreply.github.com>
2022-11-16 18:20:44 +1300

    Fixed the covid cases error

--- src/admincases.js
+++ src/admincases.js
@@ -4,49 +4,70 @@ class covidNo{
     covidno;
 
     constructor(CovidNoCases){
-        covidno = CovidNoCases;
+        this.covidno = CovidNoCases; 
     }
 }
 
-let requiredInputs = document.querySelectorAll("[required]");
-let submitButton = document.querySelector(".changeCases");
 
-submitButton.disabled = true;
-
-for(let i = 0; i < requiredInputs.length; i++){
-requiredInputs[i].addEventListener("input", buttonState)
-};
-
-function buttonState() {
-  if (requiredInputs.value === "") {
-    submitButton.disabled = true;
-  } else {
-    submitButton.disabled = false;
-  }
-}
-
-if (submitButton.disabled == false)
-{
-const submission = document.getElementById("submit");
-submission.addEventListener('click', submitEvent);
-function submitEvent()
+const submission = document.getElementById("changeCases");
+submission.addEventListener('click', submitEvent1);
+function submitEvent1()
 {
     //setting the covid data on the click event
-    const c = document.getElementById('casesInput').value;
+    const c = document.getElementById('casesInput').value; //Originally you had the id as covidNo I think? but in the html the id for the input is casesInput so changed it that, other wise you wouldn't be able to read any of the data at all!
     const covidCases = new covidNo(c); //creating a new object from the class and passing the form's data through the constructor in order to set the variables values
-
-    const fs = require('fs');
-    fs.writeFile('./covidnodata.json', JSON.stringify(covidCases), err =>{
+    
+    fs.readFile('src/covidnodata.json', 'utf-8', function(err, data){ //adds all the user info to the system //KYE HERE: Hey so here you were only writing, with json you need to raead the file, parse and save the data
         if (err) {
             console.log(err);
         }
-        const jsonString = JSON.stringify(covidCases);
-        console.log(jsonString);
+        else {
+            const file = JSON.parse(data); //So here I parsed the data from the json file
+            file.covidNo = covidCases.covidno; //Here I change the covidNo that currently exists in the file (file.covidNo accesses that), to the value you get from the html that's saved to your covid number object
+            const validJson = JSON.stringify(file); //making the data back into a json string ready to be written back to the file
 
-    });
+
+            fs.writeFile('src/covidnodata.json', validJson, 'utf-8', function(err) { //sends the data back to the file with the new, updated number
+                if(err){
+                    console.log(err); 
+                }
+                else {
+                    console.log("You have successfully changed the covid numbers."); //message just for testing purposes, you should look at making this a pop up dialog box though so the admin knows the new number has been saved
+                }
+            });
+        }
+    })
+    
 }
 
-}
+//Dunno what this is but you don't need it if its for changing the covid number
+/* // var mydata = JSON.parse(covidnodata);
+// alert(mydata[0].covidno);
+
+fetch(covidnodata.json)
+  .then(function (response) {
+    // The JSON data will arrive here
+  })
+  .catch(function (err) {
+    // If an error occured, you will catch it here
+  });
+
+  fetch('covidnodata.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+  var mainContainer = document.getElementById("CurrentCases");
+
+  for (var i = 0; i < data.length; i++) {
+  }
+   */
 
 //<!--This function will be used to change the covid numbers for the system and home page-->
 //function covidno(){
